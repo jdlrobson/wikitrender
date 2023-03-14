874ff3027eb6714124161d275bd83c669e5a8a82
Jon Robson <jdlrobson@gmail.com>
2023-03-14 16:00:31 -0700

    Improve the example code

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,5 @@
 var WikiSocketCollection = require( './../index.js' );
+const NEW_LINE = `###########################`;
 //var Trender = require( 'Trender.js' );
 
 var collection = new WikiSocketCollection( {
@@ -7,11 +8,12 @@ var collection = new WikiSocketCollection( {
 	maxAge: 60,
 	// Only keep things which are getting more than 5 edits per minute
 	minSpeed: 5,
+	debug: true,
 	clearCache: true
 } );
 
 collection.on( 'edit', function ( page, collection ) {
-	console.log( `${page.title} was edited, it is currently being edited at ${page.editsPerMinute()} edits per minute` );
+	console.log( `[info] ${page.title} was edited, it is currently being edited at ${page.editsPerMinute()} edits per minute` );
 } );
 
 setInterval( function () {
@@ -45,19 +47,26 @@ setInterval( function () {
 		} );
 	}
 
+	console.log(NEW_LINE);
 	console.log('### Most vibrant ### ');
+	console.log(NEW_LINE);
 	render( mostVibrant(), function ( page ) {
 		return page.getBias();
 	} );
 	
+	console.log(NEW_LINE);
 	console.log('### Biggest movers ### ');
+	console.log(NEW_LINE);
 	render( mostChanged(), function ( page ) {
 		return page.bytesChanged;
 	} );
 
+	console.log(NEW_LINE);
 	console.log('### Most edited ### ');
+	console.log(NEW_LINE);
 	render( mostEdited(), function ( page ) {
 		return page.editsPerMinute();
 	} );
+	console.log(NEW_LINE);
 }, 1000 * 10 );
 
